dee0710e3584729744292e531c0fe2a92e3e356e
girmesh03 <girmachewzewdie03@gmail.com>
2023-07-25 02:30:10 +0300

    Initial project setup: Installed Node.js, Jest, Babel, and ESLint
    - Cloned alx-backend-javascript repository
    - Installed Node.js version 14.x
    - Initialized package.json and added project metadata
    - Installed Jest, Babel, and ESLint dependencies
    - Added Babel and ESLint configuration files

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        node: true,
+        es6: true,
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+        ecmaVersion: 2021,
+    },
+    rules: {
+        // Add any specific ESLint rules here if needed
+    },
+};
