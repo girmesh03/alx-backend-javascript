318548630cda3b48060efececaf295189afb1239
girmesh03 <girmachewzewdie03@gmail.com>
2023-07-25 03:20:58 +0300

    dir changed 1

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,4 +1,4 @@
-// import { taskFirst, taskNext } from './0-constants.js';
-const { taskFirst, taskNext } = require('./0-constants.js');
+import { taskFirst, taskNext } from './0-constants.js';
+// const { taskFirst, taskNext } = require('../0-constants.js');
 
 console.log(`${taskFirst()} ${taskNext()}`);
--- 0x00-ES6_basic/README.md
+++ 0x00-ES6_basic/README.md
@@ -1,2 +1,2 @@
-# Backend ES6 Basics
+# 0x00. ES6 Basics
 Will be updated soon
