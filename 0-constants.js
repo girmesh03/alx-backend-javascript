01d8391222d0bfd34d71966608a0a1d41cf1bce9
girmesh03 <girmachewzewdie03@gmail.com>
2023-07-25 03:00:32 +0300

    task 0 added

--- 0-constants.js
+++ 0-constants.js
@@ -1,20 +1,15 @@
-// 0-constants.js
-
-// Modify the function to use 'const'
 export function taskFirst() {
     const task = 'I prefer const when I can.';
     return task;
 }
 
-// Modify the function to use 'let'
+export function getLast() {
+    return ' is okay';
+}
+
 export function taskNext() {
     let combination = 'But sometimes let';
     combination += getLast();
 
     return combination;
-}
-
-// Helper function - no need to modify this one
-export function getLast() {
-    return ' is okay';
 }
