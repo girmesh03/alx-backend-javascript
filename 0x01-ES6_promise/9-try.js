681dc9e69cc9b017339af5819490ef8ee3eed6e1
girmesh03 <girmachewzewdie03@gmail.com>
2023-07-27 04:00:18 +0300

    9-try.js

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,18 @@
+// 9-try.js
+
+function guardrail(mathFunction) {
+    const queue = [];
+
+    try {
+        const result = mathFunction();
+        queue.push(result);
+    } catch (error) {
+        queue.push(error.toString());
+    } finally {
+        queue.push('Guardrail was processed');
+    }
+
+    return queue;
+}
+
+export default guardrail;
