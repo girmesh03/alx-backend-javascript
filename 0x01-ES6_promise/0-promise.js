e2ad9d66c442aabaeea2ced55b454b3155e266a0
girmesh03 <girmachewzewdie03@gmail.com>
2023-07-27 03:29:23 +0300

    0-promise.js, to meet the requirements 3

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,10 @@
+/*0-promise.js*/
+
 function getResponseFromAPI() {
     return new Promise((resolve, reject) => {
-        resolve({ status: 200, body: 'Success' });
-        reject(Error('Status code is 404'));
+        setTimeout(() => {
+            resolve("API Response Data");
+        }, 1000);
     });
 }
 
