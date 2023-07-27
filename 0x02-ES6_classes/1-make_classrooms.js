e3d51361e684498bde6af73f0ec9af9db80d2503
girmesh03 <girmachewzewdie03@gmail.com>
2023-07-27 18:08:35 +0300

    1-make_classrooms.js

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+// 1-make_classrooms.js
+import ClassRoom from './0-classroom.js';
+
+function initializeRooms() {
+  const sizes = [19, 20, 34];
+  const classrooms = sizes.map(size => new ClassRoom(size));
+  return classrooms;
+}
+
+export default initializeRooms;
