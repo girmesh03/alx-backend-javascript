8a226fa338d18433e9377cf8b596d5d6b1f5a4a9
girmesh03 <girmachewzewdie03@gmail.com>
2023-07-27 04:01:35 +0300

    100-await.js

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,21 @@
+// 100-await.js
+
+import { uploadPhoto, createUser } from './utils';
+
+async function asyncUploadUser() {
+    try {
+        const [photoResponse, userResponse] = await Promise.all([uploadPhoto(), createUser()]);
+
+        return {
+            photo: photoResponse,
+            user: userResponse,
+        };
+    } catch (error) {
+        return {
+            photo: null,
+            user: null,
+        };
+    }
+}
+
+export default asyncUploadUser;
