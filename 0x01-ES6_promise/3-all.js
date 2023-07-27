ccb604dd71ade63a607f8e518ca1899514d45a78
girmesh03 <girmachewzewdie03@gmail.com>
2023-07-27 05:19:06 +0300

    3-all.js update 2

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,16 +1,11 @@
-import { uploadPhoto, createUser } from "./utils";
+import { createUser, uploadPhoto } from './utils';
 
-async function handleProfileSignup() {
-  try {
-    const [photoResponse, userResponse] = await Promise.all([
-      uploadPhoto(),
-      createUser(),
-    ]);
-
-    console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
-  } catch (error) {
-    console.log('Signup system offline');
-  }
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((data) => {
+      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
 
 export default handleProfileSignup;
