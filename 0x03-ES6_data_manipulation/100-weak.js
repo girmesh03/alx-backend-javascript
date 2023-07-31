cac145605ce866e1d3fa45320003e1b66772d288
girmesh03 <girmachewzewdie03@gmail.com>
2023-08-01 01:13:38 +0300

    100-weak.js updated #4

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,15 +1,21 @@
-const weakMap = new WeakMap();
+// 100-weak.js
 
-function queryAPI(endpoint) {
-  let queryCount = weakMap.get(endpoint) || 0;
-  queryCount++;
+// Create a WeakMap to track the number of queries for each endpoint
+export const weakMap = new WeakMap();
 
-  weakMap.set(endpoint, queryCount);
+// Export the queryAPI function
+export function queryAPI(endpoint) {
+  // Check if the endpoint exists in the weakMap, if not, set its count to 0
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
 
-  if (queryCount >= 5) {
+  // Get the current count for the endpoint and increment it
+  const currentCount = weakMap.get(endpoint);
+  weakMap.set(endpoint, currentCount + 1);
+
+  // Check if the count is >= 5, throw an error
+  if (weakMap.get(endpoint) >= 5) {
     throw new Error('Endpoint load is high');
   }
 }
-
-export { weakMap, queryAPI };
-
